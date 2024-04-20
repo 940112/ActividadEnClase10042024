c1602c87da516f2a4255ccb7b2bfb61105adcd31
Daniel Alvarez <dalvar13@ibero.edu.co>
2024-04-19 22:08:25 -0500

    termino actividad

--- scripts/PracticaConceptos.js
+++ scripts/PracticaConceptos.js
@@ -1,3 +1,34 @@
-console.log("Hola soy la axtividad de hoy 10/04/2024");
+console.log("Hola soy la axtividad de hoy 1/04/2024");
 const edadInput = document.getElementById("edadInput");
 console.log(edadInput);
+
+const mensaje = document.
+    getElementById("mensajeEdad");
+console.log(mensaje);
+
+
+
+
+function verificarEdad() {
+    console.log("entro a mi funcion verificar edad");
+
+
+    let edad = edadInput.value;
+    console.log(edad);
+
+
+
+    if (edad >= 18) {
+        console.log("Eres mayor de edad");
+        mensaje.textContent = "Eres mayor de edad";
+
+    } else {
+        console.log("Eres menor de edad");
+
+        mensaje.textContent = "Eres menor de edad";
+    }
+
+
+
+
+}
